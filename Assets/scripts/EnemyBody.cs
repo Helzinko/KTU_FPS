697f1ba633111e9c7d83b21fbcdb8f3dc1c6d5dd
Mantas Banevičius <mantas.bane@gmail.com>
2020-03-10 11:41:01 +0200

    Created prototype
    This prototype contains:
    
    Fps controler, Fps shooting, Simple level design, enemies that can shoot at you and can be shooted by you.

---
+++ Assets/scripts/EnemyBody.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBody : MonoBehaviour
+{
+    GameObject player;
+    GameObject parent;
+
+    public float seeDistance = 30;
+
+    public float bulletSpeed = 100;
+
+    public GameObject bullet;
+    public GameObject bulletHole;
+
+    float fireRate = 1f;
+    float nextFire;
+
+    bool sawPlayer = false;
+
+    void Start()
+    {
+        parent = transform.parent.gameObject;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!parent.GetComponent<enemy>().isDead)
+        {
+            //if (sawPlayer)
+                //GetComponent<NavMeshAgent>().destination = player.transform.position;
+
+            if (Vector3.Distance(transform.position, player.transform.position) < seeDistance)
+            {
+                Vector3 lookAtPosition = player.transform.position;
+                lookAtPosition.y = transform.position.y;
+                transform.LookAt(lookAtPosition);
+            }
+        }
+
+        //if (parent.GetComponent<enemy>().isDead)
+           // Destroy(GetComponent<NavMeshAgent>());
+
+
+
+        if (!parent.GetComponent<enemy>().isDead && Time.time > nextFire)
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) < seeDistance)
+            {
+                Vector3 fwd = transform.TransformDirection(Vector3.forward);
+                Debug.DrawRay(transform.position, fwd * seeDistance, Color.green);
+
+                RaycastHit objectHit;
+
+                if (Physics.Raycast(transform.position, fwd, out objectHit, seeDistance))
+                {
+                    if (objectHit.transform.tag == "Player")
+                    {
+                        sawPlayer = true;
+                        nextFire = Time.time + fireRate;
+                        GameObject newProjectile = Instantiate(bullet, bulletHole.transform.position, transform.rotation);
+                        newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+                        Destroy(newProjectile, 2f);
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    Rigidbody[] bodies;
+    public bool isDead = false;
+    GameObject player;
+
+    void Start()
+    {
+        bodies = GetComponentsInChildren<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void GotHit()
+    {
+        isDead = true;
+        for (int i = 0; i < bodies.Length; i++)
+            bodies[i].isKinematic = false;
+    }
+}
