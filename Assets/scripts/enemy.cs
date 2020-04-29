10d70532f7c5111a9ae7d9b70738d77c75e535da
Mantas Banevičius <mantas.bane@gmail.com>
2020-04-29 21:55:24 +0300

    Added enemy walking function.

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -8,14 +8,18 @@ public class enemy : MonoBehaviour
     public bool isDead = false;
     GameObject player;
 
+    Animator anim;
+
     void Start()
     {
         bodies = GetComponentsInChildren<Rigidbody>();
         player = GameObject.FindGameObjectWithTag("Player");
+        anim = gameObject.GetComponent<Animator>();
     }
 
     public void GotHit()
     {
+        anim.enabled = false;
         isDead = true;
         for (int i = 0; i < bodies.Length; i++)
             bodies[i].isKinematic = false;
